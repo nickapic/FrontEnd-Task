dfba804a91bfc795605bb037a5c83daa294d019a
nickapic <43927184+nickapic@users.noreply.github.com>
2019-12-19 23:59:24 +0200

    Most Responsivness Added

--- script/index.js
+++ script/index.js
@@ -1,10 +1,14 @@
-
+var wireframeBox = ".wireframe-conainer-itembox-list"; 
 window.onload = function getItems(){
         // Make a request for a user with a given ID
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(function (response) {
+                for(var i =0; i < 5;i++ ){
+                        var newhtml='<li class="wireframe-container-itembox-list-items">'+ response.data[i].body+'</li>'
+                        console.log(newhtml)
+                        document.querySelector('.wireframe-conainer-itembox-list').insertAdjacentHTML('beforeend',newhtml);
+                }
         // handle success
-        console.log(response);
         })
         .catch(function (error) {
         // handle error
@@ -14,3 +18,15 @@ window.onload = function getItems(){
         // always executed
 });
 }
+
+
+function addItemsToHtml(response){
+        // html= '<li class="wireframe-container-itembox-list-items">Item 1</li>'; 
+        for(var i =0; i > 5;i++ ){
+                newhtml='<li class="wireframe-container-itembox-list-items">'+ response.data[i].body+'</li>'
+                document.querySelector(wireframeBox).insertAdjacentHTML("beforeend",newhtml);
+        }
+        
+
+}
+
